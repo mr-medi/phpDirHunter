e12052550c6a1e4357093e5c8dd7be582ea171e6
MrMedi <metamail9@protonmail.ch>
2019-12-23 16:18:27 +0100

    Funciones añadidas

--- src/funciones.php
+++ src/funciones.php
@@ -1,4 +1,25 @@
 <?php
-  
+    function getDirectories($url)
+    {
+      $resultado = "";
+      $lista = file_get_contents("lista.txt");
+      $directorios = explode("\n", $lista);
+      foreach ($directorios as $directorio)
+      {
+        $recurso = $url.$directorio;
+        $resultado .= "<strong>Probando => $recurso ...</strong><br>";
+
+        if(fopen($recurso, "r"))
+        {
+          $resultado .= "[ * ]Encontrado directorio en $url<br>";
+          //file_put_contents($file, file_get_contents($file).$url.";");
+        }
+        else
+        {
+          $resultado .= "[ ! ]Nada encontrado<br>";
+        }
+      }
+      return $resultado;
+    }
 
  ?>
--- src/main.php
+++ src/main.php
@@ -2,29 +2,13 @@
     require_once "funciones.php";
  ?>
 <?php
-  $url = "https://www.youtube.com/";
-  $file = "resultado.txt";
-
-  if(!is_file($file))
+  if(isset($_POST["enviar"]))
   {
-      $contents = "";
-      file_put_contents($file, $contents);
-  }
-  $lista = file_get_contents("lista.txt");
-  $directorios = explode("\n", $lista);
-  foreach ($directorios as $directorio)
-  {
-    $recurso = $url.$directorio;
-    echo "<strong>Probando => $recurso ...</strong><br>";
-
-    if(fopen($recurso, "r"))
+    if(!empty($_POST["url"]))
     {
-      echo "[ * ]Encontrado directorio en $url<br>";
-      file_put_contents($file, file_get_contents($file).$url.";");
-    }
-    else
-    {
-      echo "[ ! ]...<br>";
-    }
-  }
+      $file = "resultado.txt";
+      $url = $_POST["url"];
+      echo getDirectories($url);
+     }
+   }
  ?>
