68df4b85d26524c2c48778950f40a5a946ae4cc8
MrMedi <metamail9@protonmail.ch>
2019-12-23 02:09:46 +0100

    Adding first files

---
+++ dirBuster/main.php
@@ -0,0 +1,30 @@
+<?php
+    require_once "funciones.php";
+ ?>
+<?php
+  $url = "http://www.dialsl.es/";
+  $file = "resultado.txt";
+
+  if(!is_file($file))
+  {
+      $contents = "";
+      file_put_contents($file, $contents);
+  }
+  $lista = file_get_contents("lista.txt");
+  $directorios = explode("\n", $lista);
+  foreach ($directorios as $directorio)
+  {
+    $recurso = $url.$directorio;
+    echo "<strong>Probando => $recurso ...</strong><br>";
+
+    if(fopen($recurso, "r"))
+    {
+      echo "[ * ]Encontrado directorio en $url<br>";
+      file_put_contents($file, file_get_contents($file).$url.";");
+    }
+    else
+    {
+      echo "[ ! ]...<br>";
+    }
+  }
+ ?>
